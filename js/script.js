593b36eec0149d25640cebb227af39e87286dcce
Stein Arild Myhr <stein_myhr@hotmail.com>
2020-11-03 19:32:25 +0100

    Added json file to project files. Made array with character names to be used

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-fetch("https://noroffcors.herokuapp.com/https://github.com/joakimskoog/AnApiOfIceAndFire/blob/master/data/characters.json")
+fetch("characters.json")
   .then(result => result.json())
   .then((res) => {
     thronesInfo(res);
@@ -6,5 +6,7 @@ fetch("https://noroffcors.herokuapp.com/https://github.com/joakimskoog/AnApiOfIc
   .catch(err => console.log(err))
 
 function thronesInfo(result) {
-  console.log(result);
+  console.log(result[1302]);
+  var characterArray = [result[1051], result[1302], result[582], result[1426], result[1934], result[1876], result[147], result[216], result[1345], result[956]];
+  console.log(characterArray);
 }
