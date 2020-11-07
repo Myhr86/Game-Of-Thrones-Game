b39be406d4a510a60eee98b1256632e182d261e1
Stein Arild Myhr <stein_myhr@hotmail.com>
2020-11-07 13:58:43 +0100

    Change gameboard layout. Add function to move token on grid

--- js/boardgame.js
+++ js/boardgame.js
@@ -1,18 +1,36 @@
 var state = localStorage.getItem('state');
 var playerOne = localStorage.getItem('playerOne');
 var playerTwo = localStorage.getItem('playerTwo');
+var p1 = "Player One";
+var p2 = "Computer";
+var activePlayer = 0;
+
 
 var diceContainer = document.getElementById("diceContainer");
 var dice = document.createElement("img");
 var h3 = document.querySelector("H3");
 var square = document.querySelectorAll(".square");
+var squareContent = document.getElementsByClassName("square");
 var gridSingleRight = document.querySelectorAll(".firstSingle");
 var gridSingleLeft = document.querySelectorAll(".secondSingle");
 
+var currentPlayer = document.getElementById("currentPlayer");
+var p1TextNode = ": It's your turn!";
+var p2TextNode = ": It's your turn!";
 var firstPlayer = document.getElementById("playerOne");
 var secondPlayer = document.getElementById("playerTwo");
 firstPlayer.innerHTML = playerOne;
 secondPlayer.innerHTML = playerTwo;
+
+
+if (activePlayer % 2 == 0) {
+  console.log("he");
+  currentPlayer.append(p1);
+  currentPlayer.append(p1TextNode);
+} else {
+  currentPlayer.append(p2);
+  currentPlayer.append(p2TextNode);
+}
 
 var img = document.createElement("img");
 img.src = "/images/testing.png";
@@ -20,19 +38,61 @@ img.classList.add("token");
 console.log(square);
 square[0].append(img);
 
-dice0.src = "/images/dice1.png";
-dice1.src = "/images/dice2.png";
-dice2.src = "/images/dice3.png";
-dice3.src = "/images/dice4.png";
-dice4.src = "/images/dice5.png";
-dice5.src = "/images/dice6.png";
-
 function throwDice() {
   var random = Math.floor((Math.random() * 6) + 1);
-  dice.src = "/images/dice" + random + ".png";
+  switch (random) {
+    case 1:
+      random = 1;
+      dice.src = "/images/dice" + 1 + ".png";
+      break;
+    case 2:
+      random = 2;
+      dice.src = "/images/dice" + 2 + ".png";
+      break;
+    case 3:
+      random = 3;
+      dice.src = "/images/dice" + 3 + ".png";
+      break;
+    case 4:
+      random = 4;
+      dice.src = "/images/dice" + 4 + ".png";
+      break;
+    case 5:
+      random = 5;
+      dice.src = "/images/dice" + 5 + ".png";
+      break;
+    case 6:
+      random = 6;
+      dice.src = "/images/dice" + 6 + ".png";
+      break;
+  }
 
   h3.style.display = "none";
   diceContainer.append(dice);
-}
 
-console.log("yolo");
+  if(activePlayer % 2 == 0) {
+    currentPlayer = firstPlayer;
+  } else {
+    currentPlayer = secondPlayer;
+  }
+
+  activePlayer++;
+  console.log(activePlayer);
+
+  move();
+  function move() {
+    var token = document.querySelector(".token");
+
+
+    for (var i = 0; i < squareContent.length; i++) {
+        var activeSquare = squareContent;
+    }
+
+    var tokenPos = activeSquare[random];
+
+    var currentPos = tokenPos;
+    tokenPos.append(token);
+    console.log(currentPos);
+    console.log(tokenPos);
+  }
+}
